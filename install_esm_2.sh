c7b7c48589de384629082edae75ec6e27f8bd056
jonazhu <jonzhu2000@gmail.com>
2024-04-21 21:07:21 -0400

    even more install edits

--- install_esm_2.sh
+++ install_esm_2.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-#shell to install ESMFold based on the guidelines posted by the TA.
-#if this is wrong, tough luck.
-echo "Getting ESMFold files..."
-git clone https://github.com/facebookresearch/esm.git
-echo "Files retrieved successfully."
-echo "Creating environment..."
-conda env create -f esm/environment.yml
-echo "Environment creation successful."
-conda activate esmfold
-
 echo "Loading modules..."
 conda install -c conda-forge einops=0.6.1
 module load cuda/11.7.1
