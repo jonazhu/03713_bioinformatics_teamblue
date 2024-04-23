c3694d27756cbadf91181f2edb3546f102c7fae1
jonazhu <jonzhu2000@gmail.com>
2024-04-23 13:29:52 -0400

    copy step

--- protpred_bridges.sh
+++ protpred_bridges.sh
@@ -12,7 +12,11 @@ python step1_final.py $1 $2 $3
 echo "Protein sequences obtained."
 echo "Running ESMFold..."
 mkdir $4
+cp $2 $PROJECT/$4
 cd $4
+
+#to add: step that copies the list file here
+#this is specifically for one-change things
 bash ../step2_esm.sh ../homomer.fasta
 cd ../
 echo "ESMFold finished running. Use resulting directory of PDB files on a local machine with PyMol."
