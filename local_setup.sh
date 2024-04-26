b95fb198def9f034150f4c1f5136db87d38ac72e
AndrewLutsky <andrew.lutsky19@gmail.com>
2024-04-26 18:06:45 -0400

    updated local setup

--- local_setup.sh
+++ local_setup.sh
@@ -5,4 +5,5 @@ conda install pip
 CONDA_SUBDIR=osx-64 conda env update -n protpredlocal --file local_env.yml
 pip install --use-pep517 lightdock
 pip install --use-pep517 prody
+pip install pyparsing==3.1.1
 chmod +x protpred_local.sh
